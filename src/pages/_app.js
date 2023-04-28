c2a6dbfe2a7325b31c96a283c5b204bee80e63ef
Sandro Huber <sandro@sh-digital.ch>
2023-04-28 17:34:17 +0200

    scroll top and progress bar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,29 @@
 import '@/styles/globals.scss'
 import MainLayout from "../layouts/Main";
 import {AnimatePresence} from "framer-motion"
+import ProgressBar from "@badrap/bar-of-progress";
+import Router from "next/router";
+import ScrollToTop from "react-scroll-to-top";
+import { FiArrowUp } from 'react-icons/fi';
+
+const progress = new ProgressBar({
+  size: 2,
+  color: "#1B2431",
+  className: "bar-of-progress",
+  delay: 100,
+});
+
+
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("routeChangeComplete", progress.finish);
+Router.events.on("routeChangeError", progress.finish);
 
 export default function App({Component, pageProps}) {
     return (
         <MainLayout>
             <AnimatePresence>
                 <Component {...pageProps} />
+               <ScrollToTop smooth component={<p  style={{ backgroundColor:"FF7A00", display: "flex", justifyContent: "center", alignItems:"center"}}><FiArrowUp /></p>} />
             </AnimatePresence>
         </MainLayout>
     )
