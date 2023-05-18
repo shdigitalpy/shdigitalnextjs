eb31ae56a7ce2648e85bbe2bd9cacb8e773ea601
Sandro Huber <sandro@sh-digital.ch>
2023-05-18 14:08:36 +0200

    change structured data

--- src/components/common/BreadCrumb/index.js
+++ src/components/common/BreadCrumb/index.js
@@ -7,22 +7,19 @@ import Image from "next/image";
 const BreadCrumb = ({ category = '', categoryUrl, post = '' }) => {
     return (
         <div className="bread-crumb" itemScope itemType="https://schema.org/BreadcrumbList">
-            <div itemprop="itemListElement" itemscope
-          itemtype="https://schema.org/ListItem">
-            <Link itemProp="item" href={`${categoryUrl}`} title={ category } className="link">
-                <p itemProp="name">{ category }</p>
+            
+            <Link itemprop="itemListElement" itemtype="https://schema.org/ListItem" href={`${categoryUrl}`} title={ category } className="link">
+                <p itemscope itemProp="item" itemProp="name">{ category }</p>
                 <meta itemProp="position" content="1" />
             </Link>
-            </div>
+         
             <Image src={chevronRight} alt="arrow right" className="link-right-icon" />
             
-            <div itemprop="itemListElement" itemscope
-          itemtype="https://schema.org/ListItem">
-            <Link itemProp="item" href={`${post.slug}`} title={ post.title } className="post-title">
-            <p itemProp="name" className="post-title">{ post.shortTitle }</p>
+            <Link itemprop="itemListElement" itemscope
+          itemtype="https://schema.org/ListItem" href={`${post.slug}`} title={ post.title } className="post-title">
+            <p itemProp="item" itemProp="name" className="post-title">{ post.shortTitle }</p>
             <meta itemProp="position" content="2" />
             </Link>
-            </div>
         
         </div>
     );
