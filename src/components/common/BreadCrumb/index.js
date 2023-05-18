3b0e2e0c32602e436ca511eedb37605b24efa031
Sandro Huber <sandro@sh-digital.ch>
2023-05-18 14:21:42 +0200

    breadcrumb list

--- src/components/common/BreadCrumb/index.js
+++ src/components/common/BreadCrumb/index.js
@@ -7,23 +7,26 @@ import Image from "next/image";
 const BreadCrumb = ({ category = '', categoryUrl, post = '' }) => {
     return (
         <div className="bread-crumb" itemScope itemType="https://schema.org/BreadcrumbList">
-            
-            <Link itemprop="itemListElement" itemtype="https://schema.org/ListItem" href={`${categoryUrl}`} title={ category } className="link">
-                <div itemscope itemProp="item">
-                <p itemProp="name">{ category }</p>
-                </div>
-                <meta itemProp="position" content="1" />
+            <ul style={{ display: 'flex', listStyle: 'none' }}>
+            <li style={{ display: 'inline-block', whiteSpace: 'nowrap'}} itemprop="itemListElement" itemtype="https://schema.org/ListItem">
+            <Link itemscope itemProp="item" href={`${categoryUrl}`} title={ category } className="link">
+                
+            <p itemProp="name">{ category }</p>
+            <meta itemProp="position" content="1" />
             </Link>
+            </li>
          
             <Image src={chevronRight} alt="arrow right" className="link-right-icon" />
-            
-            <Link itemprop="itemListElement" itemscope
-          itemtype="https://schema.org/ListItem" href={`${post.slug}`} title={ post.title } className="post-title">
-            <div itemscope itemProp="item">
+                
+            <li style={{ display: 'inline-block',whiteSpace: 'nowrap'}} itemprop="itemListElement" itemtype="https://schema.org/ListItem">
+            <Link itemscope itemProp="item" href={`${post.slug}`} title={ post.title } className="post-title">
             <p itemProp="name" className="post-title">{ post.shortTitle }</p>
-            </div>
+           
             <meta itemProp="position" content="2" />
             </Link>
+             </li>
+
+             </ul> 
         
         </div>
     );
