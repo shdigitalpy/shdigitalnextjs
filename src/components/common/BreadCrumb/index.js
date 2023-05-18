ae0883963a2c115e49f25330a8ab7a3bd427e6c7
Sandro Huber <sandro@sh-digital.ch>
2023-05-18 13:58:15 +0200

    add listelement

--- src/components/common/BreadCrumb/index.js
+++ src/components/common/BreadCrumb/index.js
@@ -7,15 +7,23 @@ import Image from "next/image";
 const BreadCrumb = ({ category = '', categoryUrl, post = '' }) => {
     return (
         <div className="bread-crumb" itemScope itemType="https://schema.org/BreadcrumbList">
+            <div itemprop="itemListElement" itemscope
+          itemtype="https://schema.org/ListItem">
             <Link itemProp="item" href={`${categoryUrl}`} title={ category } className="link">
                 <p itemProp="name">{ category }</p>
                 <meta itemProp="position" content="1" />
             </Link>
+            </div>
             <Image src={chevronRight} alt="arrow right" className="link-right-icon" />
+            
+            <div itemprop="itemListElement" itemscope
+          itemtype="https://schema.org/ListItem">
             <Link itemProp="item" href={`${post.slug}`} title={ post.title } className="post-title">
             <p itemProp="name" className="post-title">{ post.shortTitle }</p>
             <meta itemProp="position" content="2" />
             </Link>
+            </div>
+        
         </div>
     );
 };
