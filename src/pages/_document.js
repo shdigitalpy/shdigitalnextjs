73f5e1f5b92c00cbcb742fb8c5dad7fc001c9ca3
Sandro Huber <sandro@sh-digital.ch>
2023-06-16 15:58:51 +0200

    robots noindex weg

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -107,7 +107,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <meta name="robots" content="noindex" />
+      
       <link rel="icon" href="/favicon.ico"/>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
