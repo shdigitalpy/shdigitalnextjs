646c2f6c9b16b3eaaa4aa03c49b5d3b527e2a500
Sandro Huber <sandro@sh-digital.ch>
2023-06-16 15:45:06 +0200

    google tag manager

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -143,9 +143,18 @@ export default function Document() {
                 type="application/ld+json"
                 dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredDataBCTop) }}
               />
+
+
+              <Script id="gtag-script" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-MFG8F2H');`}}></Script>
         </Head>
 
       <body>
+      <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MFG8F2H"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
         <Main />
         <NextScript />
 
