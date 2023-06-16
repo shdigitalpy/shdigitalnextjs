1eb648c0fe86a20cd8cbf0e80f525ae91db9c1e4
Sandro Huber <sandro@sh-digital.ch>
2023-06-16 18:17:04 +0200

    viewport

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,8 +30,7 @@ export default function Home() {
             <Head>
                 <title>{pageTitle}</title>
                 <meta name="description" content={metaDescription}/>
-                <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                <link rel="icon" href="/favicon.ico"/>
+                
             </Head>
             <main>
                 <Hero/>
